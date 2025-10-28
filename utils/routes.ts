560d6e610c57289a288444c6e13a1d9b961ab60a
tineojf <92743762+tineodev@users.noreply.github.com>
2025-10-28 11:22:53 -0500

    add: define ROUTES for navigation in the application

---
+++ utils/routes.ts
@@ -0,0 +1,18 @@
+export const ROUTES = {
+  HOME: {
+    NAME: "Index",
+    PATH: "index",
+  },
+  CART: {
+    NAME: "Cart",
+    PATH: "cart",
+  },
+  PROFILE: {
+    NAME: "Profile",
+    PATH: "profile",
+  },
+  DASHBOARD: {
+    NAME: "Dashboard",
+    PATH: "dashboard",
+  },
+};
