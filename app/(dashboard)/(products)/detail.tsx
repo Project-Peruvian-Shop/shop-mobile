e2f8e59c917586d299dc6b4f5e2b1d932c2680ea
tineojf <92743762+tineodev@users.noreply.github.com>
2025-10-28 14:10:54 -0500

    feat: add Detail screen for product information

---
+++ app/(dashboard)/(products)/detail.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Detail() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Detail Screen</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
