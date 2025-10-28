b8cbb70f8cd6db241d8913dc564666628920b333
tineojf <92743762+tineodev@users.noreply.github.com>
2025-10-28 16:55:00 -0500

    feat: update API URL in constants and add helpful comments

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,1 +1,4 @@
-export const URL_API = "http://localhost:8080/api/v1";
+// comando para saber la ip de la pc en linux
+// ifconfig | grep inet
+
+export const URL_API = "http://192.168.18.7:8080/api/v1";
