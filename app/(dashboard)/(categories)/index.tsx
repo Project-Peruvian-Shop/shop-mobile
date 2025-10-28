2b0ad37d9e287819a42b8a8231959d6fec6d044b
tineojf <92743762+tineodev@users.noreply.github.com>
2025-10-28 16:57:41 -0500

    feat: implement category fetching and display with loading state

--- app/(dashboard)/(categories)/index.tsx
+++ app/(dashboard)/(categories)/index.tsx
@@ -1,15 +1,59 @@
+import { CategoriaDashboardDTO } from "@/models/Categoria/Categoria_response";
+import { getAllCategories } from "@/services/categoria.service";
+import { PaginatedResponse } from "@/services/global.interfaces";
 import { ROUTES } from "@/utils/routes";
 import { router } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
 
 export default function Categories() {
+  const [loading, setLoading] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [cantidad, setCantidad] = useState(0);
+
+  const [categorias, setCategorias] =
+    useState<PaginatedResponse<CategoriaDashboardDTO>>();
+
+  useEffect(() => {
+    fetchAll();
+  }, []);
+
+  const fetchAll = async (page: number = 0) => {
+    setLoading(true);
+    try {
+      const res = await getAllCategories(page);
+      setCategorias(res);
+      setTotalPages(res.totalPages);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <View>
-      <Text>Categorias Screen</Text>
-      <Button
-        title="ID: 123"
-        onPress={() => router.push(ROUTES.DASHBOARD.CATEGORIES.DETAIL.GO(1))}
-      />
+    <View style={{ padding: 16 }}>
+      <Text style={{ fontSize: 18, fontWeight: "bold", marginBottom: 10 }}>
+        Categorías
+      </Text>
+
+      {loading ? (
+        <Text>Cargando...</Text>
+      ) : (
+        <FlatList
+          data={categorias?.content ?? []}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onPress={() =>
+                router.push(ROUTES.DASHBOARD.CATEGORIES.DETAIL.GO(item.id))
+              }
+            >
+              <Text style={{ fontSize: 16, marginVertical: 8 }}>
+                {item.nombre}
+              </Text>
+            </TouchableOpacity>
+          )}
+        />
+      )}
     </View>
   );
 }
