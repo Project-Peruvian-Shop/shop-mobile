b524d51f9db54893869c960109b209a69f366384
tineojf <92743762+tineodev@users.noreply.github.com>
2025-10-29 10:42:10 -0500

    feat: refactor Categories component to use DashboardTable and remove FlatList

--- app/(dashboard)/(categories)/index.tsx
+++ app/(dashboard)/(categories)/index.tsx
@@ -1,15 +1,19 @@
+import {
+  Action,
+  Column,
+  DashboardTable,
+} from "@/components/dashboard/table/table";
 import { CategoriaDashboardDTO } from "@/models/Categoria/Categoria_response";
 import { getAllCategories } from "@/services/categoria.service";
 import { PaginatedResponse } from "@/services/global.interfaces";
 import { ROUTES } from "@/utils/routes";
 import { router } from "expo-router";
 import { useEffect, useState } from "react";
-import { FlatList, Text, TouchableOpacity, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Categories() {
   const [loading, setLoading] = useState(false);
   const [totalPages, setTotalPages] = useState(0);
-  const [cantidad, setCantidad] = useState(0);
 
   const [categorias, setCategorias] =
     useState<PaginatedResponse<CategoriaDashboardDTO>>();
@@ -24,10 +28,35 @@ export default function Categories() {
       const res = await getAllCategories(page);
       setCategorias(res);
       setTotalPages(res.totalPages);
+      console.log(res);
+      
     } finally {
       setLoading(false);
     }
   };
+
+  const columns: Column<CategoriaDashboardDTO>[] = [
+    { header: "ID", accessor: "id" },
+    { header: "Nombre", accessor: "nombre" },
+    { header: "Usos", accessor: "usos" },
+    {
+      header: "Norma",
+      accessor: "norma",
+      render: (value) => (value ? "Activo" : "Inactivo"),
+    },
+  ];
+
+  const actions: Action<CategoriaDashboardDTO>[] = [
+    {
+      label: "Editar",
+      onPress: (row) =>
+        router.push(ROUTES.DASHBOARD.CATEGORIES.DETAIL.GO(row.id)),
+    },
+    {
+      label: "Eliminar",
+      onPress: (row) => console.log("Eliminar", row.id),
+    },
+  ];
 
   return (
     <View style={{ padding: 16 }}>
@@ -38,20 +67,13 @@ export default function Categories() {
       {loading ? (
         <Text>Cargando...</Text>
       ) : (
-        <FlatList
+        <DashboardTable<CategoriaDashboardDTO>
+          columns={columns}
           data={categorias?.content ?? []}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              onPress={() =>
-                router.push(ROUTES.DASHBOARD.CATEGORIES.DETAIL.GO(item.id))
-              }
-            >
-              <Text style={{ fontSize: 16, marginVertical: 8 }}>
-                {item.nombre}
-              </Text>
-            </TouchableOpacity>
-          )}
+          actions={actions}
+          currentPage={categorias?.number ?? 0}
+          totalPages={totalPages}
+          onPageChange={fetchAll}
         />
       )}
     </View>
