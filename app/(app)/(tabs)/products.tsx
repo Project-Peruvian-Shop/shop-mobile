20c745705c71ff501b43f9b8a9395254a3408427
tineojf <92743762+tineodev@users.noreply.github.com>
2025-11-01 15:57:43 -0500

    feat: replace Title component with Text for product header in Products screen

--- app/(app)/(tabs)/products.tsx
+++ app/(app)/(tabs)/products.tsx
@@ -1,10 +1,31 @@
-import { Title } from "@/components/app/title/title";
+import { SearchBar } from "@/components/dashboard/search/search";
+import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Products() {
   return (
     <SafeAreaView edges={["top", "left", "right"]}>
-      <Title title="Productos" textAlign="left" />
+      <Text style={styles.title}>Productos</Text>
+
+      <View style={styles.searchContainer}>
+        <SearchBar
+          placeholder="Buscar productos..."
+          value=""
+          onChangeText={() => {}}
+        />
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    paddingTop: 16,
+    paddingHorizontal: 16,
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  searchContainer: {
+    marginHorizontal: 16,
+  },
+});
