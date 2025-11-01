24ff3dabc273171796a88f93042918f4d78f5e9b
tineojf <92743762+tineodev@users.noreply.github.com>
2025-11-01 16:15:05 -0500

    feat: implement product fetching logic and loading state in Products screen

--- app/(app)/(tabs)/products.tsx
+++ app/(app)/(tabs)/products.tsx
@@ -1,8 +1,33 @@
 import { SearchBar } from "@/components/dashboard/search/search";
+import { PaginatedProductoResponseDTO } from "@/models/Producto/Producto_response_dto";
+import { PaginatedResponse } from "@/services/global.interfaces";
+import { getPaginatedProductos } from "@/services/producto.service";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Products() {
+  const [loading, setLoading] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const [productos, setProductos] =
+    useState<PaginatedResponse<PaginatedProductoResponseDTO>>();
+
+  useEffect(() => {
+    fetchAll();
+  }, []);
+
+  const fetchAll = async (page: number = 0) => {
+    setLoading(true);
+    try {
+      const res = await getPaginatedProductos(page);
+      setProductos(res);
+      setTotalPages(res.totalPages);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <SafeAreaView edges={["top", "left", "right"]}>
       <Text style={styles.title}>Productos</Text>
@@ -14,6 +39,8 @@ export default function Products() {
           onChangeText={() => {}}
         />
       </View>
+
+      {/* renderizar los productos con ItemCard */}
     </SafeAreaView>
   );
 }
