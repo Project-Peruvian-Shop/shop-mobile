e610cc1815845213d29f76315ff77a501fcd693f
tineojf <92743762+tineodev@users.noreply.github.com>
2025-11-02 00:04:04 -0500

    feat: implement DetailProduct component for product detail display

---
+++ app/(store)/detailproduct.tsx
@@ -0,0 +1,10 @@
+import { TitlePage } from "@/components/app/titlepage/titlepage";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function DetailProduct() {
+  return (
+    <SafeAreaView edges={["top", "left", "right"]} style={{ flex: 1 }}>
+      <TitlePage title="Detalle de Producto" />
+    </SafeAreaView>
+  );
+}
