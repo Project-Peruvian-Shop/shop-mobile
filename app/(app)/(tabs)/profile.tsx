2ebd58d0bf42011bf6d40230b638ee264b03d77d
tineojf <92743762+tineodev@users.noreply.github.com>
2025-11-02 00:23:27 -0500

    feat: add navigation buttons to Profile component for improved user access

--- app/(app)/(tabs)/profile.tsx
+++ app/(app)/(tabs)/profile.tsx
@@ -1,10 +1,71 @@
 import { TitlePage } from "@/components/app/titlepage/titlepage";
+import { ROUTES } from "@/utils/routes";
+import { useRouter } from "expo-router";
+import { Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Profile() {
+  const router = useRouter();
+
   return (
     <SafeAreaView edges={["top", "left", "right"]} style={{ flex: 1 }}>
       <TitlePage title="Perfil" />
+
+      <Button
+        title="Mi perfil"
+        onPress={() => {
+          router.push(ROUTES.PROFILE.PROFILE.ENTIRE_PATH);
+        }}
+      />
+
+      <Button
+        title="Mis cotizaciones"
+        onPress={() => {
+          router.push(ROUTES.PROFILE.COTIZACIONES.ENTIRE_PATH);
+        }}
+      />
+
+      <Button
+        title="¿Quiénes somos?"
+        onPress={() => {
+          router.push(ROUTES.LANDING.ABOUT.ENTIRE_PATH);
+        }}
+      />
+
+      <Button
+        title="Preguntas frecuentes"
+        onPress={() => {
+          router.push(ROUTES.LANDING.FAQ.ENTIRE_PATH);
+        }}
+      />
+
+      <Button
+        title="Contáctanos"
+        onPress={() => {
+          router.push(ROUTES.LANDING.CONTACT.ENTIRE_PATH);
+        }}
+      />
+
+      <Button
+        title="Libro de reclamaciones"
+        onPress={() => {
+          router.push(ROUTES.LANDING.CLAIMS.ENTIRE_PATH);
+        }}
+      />
+
+      <Button
+        title="Términos y condiciones"
+        onPress={() => {
+          router.push(ROUTES.LANDING.TERMS.ENTIRE_PATH);
+        }}
+      />
+
+      <Button
+        title="Política de privacidad"
+        onPress={() => {
+          router.push(ROUTES.LANDING.PRIVACY.ENTIRE_PATH);
+        }}
+      />
     </SafeAreaView>
   );
 }
