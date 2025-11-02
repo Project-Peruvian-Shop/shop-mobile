275a1822f19aa16d8217fd5b2438a21160b71002
tineojf <92743762+tineodev@users.noreply.github.com>
2025-11-01 23:21:07 -0500

    feat: replace Text component with TitlePage for improved header presentation in Dashboard

--- app/(app)/(tabs)/dashboard.tsx
+++ app/(app)/(tabs)/dashboard.tsx
@@ -1,31 +1,30 @@
+import { TitlePage } from "@/components/app/titlepage/titlepage";
 import { ROUTES } from "@/utils/routes";
 import { useRouter } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Dashboard() {
   const router = useRouter();
 
   return (
-    <SafeAreaView edges={["top", "left", "right"]}>
-      <View>
-        <Text>Dashboard Screen</Text>
+    <SafeAreaView edges={["top", "left", "right"]} style={{ flex: 1 }}>
+      <TitlePage title="Dashboard" />
 
-        <Button
-          title="Ir a Principal"
-          onPress={() => router.push(ROUTES.DASHBOARD.PRINCIPAL.ENTIRE_PATH)}
-        />
+      <Button
+        title="Ir a Principal"
+        onPress={() => router.push(ROUTES.DASHBOARD.PRINCIPAL.ENTIRE_PATH)}
+      />
 
-        <Button
-          title="Ir a Login"
-          onPress={() => router.push(ROUTES.AUTH.LOGIN.ENTIRE_PATH)}
-        />
+      <Button
+        title="Ir a Login"
+        onPress={() => router.push(ROUTES.AUTH.LOGIN.ENTIRE_PATH)}
+      />
 
-        <Button
-          title="Ir a Register"
-          onPress={() => router.push(ROUTES.AUTH.REGISTER.ENTIRE_PATH)}
-        />
-      </View>
+      <Button
+        title="Ir a Register"
+        onPress={() => router.push(ROUTES.AUTH.REGISTER.ENTIRE_PATH)}
+      />
     </SafeAreaView>
   );
 }
