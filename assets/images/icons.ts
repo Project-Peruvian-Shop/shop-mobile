e89ab72db62a110b886c3ad859c922796dfef551
joaoDevL <joao.software.dev@gmail.com>
2025-11-24 10:21:54 -0500

    feat: Adding the alert component and icons for each situation

--- assets/images/icons.ts
+++ assets/images/icons.ts
@@ -1,6 +1,8 @@
 export const Icons = {
   trashIcon: require("./Icons/trash.png"),
   successIcon: require("./Icons/success.png"),
+  infoIcon: require("./Icons/info.png"),
+  errorIcon: require("./Icons/error.png"),
 };
 
 export type IconName = keyof typeof Icons;
