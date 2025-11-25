0dfd3909724fefae62cab8e40dbfa08cfa443a6c
joaoDevL <joao.software.dev@gmail.com>
2025-11-25 09:55:01 -0500

    styles: Adding the image for the profile

--- assets/images/icons.ts
+++ assets/images/icons.ts
@@ -3,6 +3,7 @@ export const Icons = {
   successIcon: require("./Icons/success.png"),
   infoIcon: require("./Icons/info.png"),
   errorIcon: require("./Icons/error.png"),
+  userProfileIcon: require("./Icons/user_profile.png"),
 };
 
 export type IconName = keyof typeof Icons;
