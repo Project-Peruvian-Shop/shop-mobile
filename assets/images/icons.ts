d8071a2273bbe9579ba69d2a5c7a0ba7b9486f45
joaoDevL <joao.software.dev@gmail.com>
2025-11-26 00:36:24 -0500

    styles: Adding images to who we are

--- assets/images/icons.ts
+++ assets/images/icons.ts
@@ -5,6 +5,14 @@ export const Icons = {
   errorIcon: require("./Icons/error.png"),
   userProfileIcon: require("./Icons/user_profile.png"),
   warningIcon: require("./Icons/warning.png"),
+  compromisoIcon: require("./About_company/compromiso.png"),
+  responsabilidadIcon: require("./About_company/responsabilidad.png"),
+  excelenciaIcon: require("./About_company/excelencia.png"),
+  trabajoEnEquipoIcon: require("./About_company/trabajo_en_equipo.png"),
+  innovacionIcon: require("./About_company/innovacion.png"),
+  misionIcon: require("./About_company/mision.png"),
+  visionIcon: require("./About_company/vision.png"),
+  aboutIcon: require("./About_company/user_icon.png"),
 };
 
 export type IconName = keyof typeof Icons;
